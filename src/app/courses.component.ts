0a10ccdb41037310bc4a70ee2a0dd07266d55cf8
Govind Kumar <govindkumar3006@gmail.com>
2021-05-30 16:13:21 +0530

    templates addition

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,8 +3,11 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Cousrses</h2>'
+    template: '<h2>{{"Title: "+title}}</h2><p>{{getTitle()}}</p>'
 })
 export class CoursesComponent{
-    
+title= 'List of courses';
+getTitle(){
+    return this.title;
+}
 } 
