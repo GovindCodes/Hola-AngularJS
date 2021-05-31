b492d03534b39f2536152c69f523f3df6d7a7abe
Govind Kumar <govindkumar3006@gmail.com>
2021-05-31 09:21:10 +0530

    Class Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -12,7 +12,9 @@ import { CoursesService } from './courses.service';
       </li>
     </ul>
     <img src = "{{imgUrl}}" />
-<img [src]= "imgUrl" />
+    <img [src]= "imgUrl" />
+    <br />
+    <button class="btn btn-primary" [class.active]= "isActive">save</button>
   `,
 })
 export class CoursesComponent {
@@ -22,6 +24,7 @@ export class CoursesComponent {
   }
   imgUrl = "https://media-exp1.licdn.com/dms/image/C4E22AQFI7jj6uO-WaA/feedshare-shrink_800/0/1622372581327?e=1625097600&v=beta&t=xKnyWZh-OqVnBxP0mKBUH4UqKzGEoBcFF6HaFJkfJy8";
   courses;
+  isActive = true;
   constructor(service: CoursesService) {
     // let service = new CoursesService(); // bekar way
     this.courses = service.getCourses();
