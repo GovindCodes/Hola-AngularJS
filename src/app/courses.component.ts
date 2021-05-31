4e64c6a8d30bc21c4c4752ebd298348e19032c8f
Govind Kumar <govindkumar3006@gmail.com>
2021-05-31 08:57:24 +0530

    Dependency Injection

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,23 +1,26 @@
-
-import {Component} from '@angular/core'
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
-    selector: 'courses',
-    template: `
-    <h2>{{"Title: "+title}}</h2>
-    <p>{{getTitle()}}</p>
+  selector: 'courses',
+  template: `
+    <h2>{{ 'Title: ' + title }}</h2>
+    <p>{{ getTitle() }}</p>
     <ul>
-        <li *ngFor="let course of courses">
-            {{course}}
-        </li>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
     </ul>
-    `
+  `,
 })
-export class CoursesComponent{
-title= 'List of courses';
-getTitle(){
+export class CoursesComponent {
+  title = 'List of courses';
+  getTitle() {
     return this.title;
+  }
+  courses;
+  constructor(service: CoursesService) {
+    // let service = new CoursesService(); // bekar way
+    this.courses = service.getCourses();
+  }
 }
-courses;
-
-} 
